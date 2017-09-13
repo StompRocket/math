25565cfb61e2fab648e10fd440282b09531a32af
Rosho123 <Rosho123@users.noreply.github.com>
2017-09-12 17:23:00 -0700

    inital commit

---
+++ main.js
@@ -0,0 +1,33 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    equation: '',
+    result: ''
+  },
+  methods: {
+    evaluate: function (store) {
+      try {
+        if (this.equation) {
+          var result = math.eval(this.equation)
+          if (!JSON.stringify(result).indexOf('function') >= 0) {
+            if (result.entries) {
+              this.result = result.entries
+            } else {
+              this.result = result
+            }
+            console.log(store)
+            if (store) {
+              $('#oldResults').prepend('<p>' + this.equation + ' = ' + this.result + '</p>')
+            }
+          } else {
+            this.result = ''
+          }
+        } else {
+          this.result = ''
+        }
+      } catch (err) {
+        // window.alert(err)
+      }
+    }
+  }
+})
