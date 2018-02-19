75bd01317553349427fdd719fbca5e2c47d657a5
Ronan <rsf.sho@gmail.com>
2018-02-19 13:49:06 -0800

    layout

---
+++ src/components/app.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="page app">
+  <form class="equationForm" v-on:submit.prevent>
+    <input class="equationInput" type="text" name="equation" value="" placeholder="Type Equation Here">
+  </form>
+</div>
+</template>
+<script>
+import '../assets/app.css'
+export default {
+  name: "app",
+  data: () => ({
+
+  })
+}
+</script>
